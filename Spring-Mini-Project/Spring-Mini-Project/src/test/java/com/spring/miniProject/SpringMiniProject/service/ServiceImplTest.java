aae328ef37397122c857d22dc66d01bf8535c143
Tushar Gupta <tushartg600@gmail.com>
2023-08-05 00:26:45 +0530

    test file added

---
+++ Spring-Mini-Project/Spring-Mini-Project/src/test/java/com/spring/miniProject/SpringMiniProject/service/ServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.spring.miniProject.SpringMiniProject.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceImplTest {
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void deleteProductById() {
+    }
+
+    @Test
+    void placeOrder() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+}
